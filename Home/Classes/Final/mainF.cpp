595a0a406f7a1a66b2ee2ab44b566af71339b939
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-08-30 22:01:21 -0700

    Physics & map

--- Home/Classes/Final/mainF.cpp
+++ Home/Classes/Final/mainF.cpp
@@ -4,7 +4,19 @@ USING_NS_CC;
 
 Scene* MainF::createScene()
 {
-	return MainF::create();
+	auto ret = new (std::nothrow) MainF();
+	if (ret && ret->initWithPhysics() && ret->init())
+	{
+		ret->autorelease();
+		return ret;
+	}
+	else
+	{
+		CC_SAFE_DELETE(ret);
+		return nullptr;
+	}
+
+	return ret;
 }
 
 // Print useful error message instead of segfaulting when files are not there.
@@ -22,6 +34,10 @@ bool MainF::init()
 
 	Vec2 midlePos = Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height / 2);
 
+	auto level = TMXTiledMap::create("tmx/TestMap.tmx");
+	this->addChild(level);
+	SetPhysicsMap(level);
+
 	this->scheduleUpdate();
 
 	return true;
@@ -36,4 +52,66 @@ void MainF::update(float dt)
 void MainF::menuCloseCallback(Ref* pSender)
 {
 	Director::getInstance()->end();
+}
+
+void MainF::SetPhysicsMap(TMXTiledMap* map)
+{
+	auto physicsWorld = getPhysicsWorld();
+	physicsWorld->setGravity(cocos2d::Vec2(0, -980));
+
+	physicsWorld->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+
+	auto collicionFloor = map->getLayer("Tile Layer");
+
+	for (int row = 0; row < map->getMapSize().height; ++row)
+	{
+		for (int col = 0; col < map->getMapSize().width; ++col)
+		{
+			auto tile = collicionFloor->getTileAt(cocos2d::Vec2(col, row));
+
+			if (tile)
+			{
+				auto physicsBody = cocos2d::PhysicsBody::createBox(tile->getContentSize(), PHYSICSSHAPE_MATERIAL_DEFAULT);
+				physicsBody->setDynamic(false);
+				physicsBody->setCategoryBitmask(2);
+				physicsBody->setCollisionBitmask(1); 
+				physicsBody->setContactTestBitmask(1);
+				tile->setPhysicsBody(physicsBody);
+			}
+		}
+	}
+
+	/*auto contactListener = EventListenerPhysicsContact::create();
+	contactListener->onContactBegin = [=](PhysicsContact& contact) -> bool
+	{
+		auto a = contact.getShapeA()->getBody();
+		auto b = contact.getShapeB()->getBody();
+
+		auto other = marioPhysicsBody == a ? b : a;
+
+		if (marioPhysicsBody->getPosition().y > other->getPosition().y && abs(contact.getContactData()->normal.y) > 0.9f)
+		{
+			contacts.push_back(other);
+		}
+
+		return true;
+	};
+	contactListener->onContactSeparate = [=](PhysicsContact& contact)
+	{
+		auto a = contact.getShapeA()->getBody();
+		auto b = contact.getShapeB()->getBody();
+
+		auto separate = marioPhysicsBody == a ? b : a;
+
+		for (int i = 0; i < contacts.size(); ++i)
+		{
+			if (contacts[i] == separate)
+			{
+				contacts[i] = contacts[contacts.size() - 1];
+				contacts.pop_back();
+				break;
+			}
+		}
+	};
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);*/
 }
--- Home/Classes/Final/mainF.h
+++ Home/Classes/Final/mainF.h
@@ -17,8 +17,8 @@ public:
 	CREATE_FUNC(MainF)
 
 private:
-
 	cocos2d::Vec2 origin;
 	cocos2d::Vec2 screenSize;
 
+	void SetPhysicsMap(cocos2d::TMXTiledMap* map);
 };
